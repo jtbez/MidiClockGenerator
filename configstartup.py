cd469ebddf0984a51f6aa80eed9f2b224d705609
Joe Berry <accounts@joeberry.info>
2026-06-06 11:22:49 +0100

    Fix ini file path in configstartup.py for Linux/macOS

--- configstartup.py
+++ configstartup.py
@@ -1,17 +1,22 @@
+import os
+import sys
 from configparser import ConfigParser
 from kivy.config import Config
 from kivy.utils import platform
 """
-configstartup.py is used to set graphics 
+configstartup.py is used to set graphics
 
-This code must be at the top of the 'main' executable file.  
+This code must be at the top of the 'main' executable file.
 
-Config.set('graphics'...) are used to set the size of the main window. 
+Config.set('graphics'...) are used to set the size of the main window.
 """
 # Default window size and position, also used to set minimum window size
 if platform == 'macosx':
     window_width = 175
     window_height = 550
+elif platform == 'linux':
+    window_width = 222
+    window_height = 600
 else:
     window_width = 222
     window_height = 600
@@ -19,9 +24,15 @@ else:
 window_top = 100
 window_left = 100
 
+# Resolve the ini file path — on Windows it sits next to the exe; on Linux/macOS it lives in ~
+if sys.platform == 'win32':
+    _ini_path = 'midiclock.ini'
+else:
+    _ini_path = os.path.expanduser('~/.midiclock.ini')
+
 # Use Python lib configparser to read .ini file prior to app startup
 parser = ConfigParser()
-found = parser.read('midiclock.ini')  # created in main.py: build_config()
+found = parser.read(_ini_path)  # created in main.py: build_config()
 if found:
     Config.set('graphics', 'width', parser['Window']['width'])
     Config.set('graphics', 'height', parser['Window']['height'])
